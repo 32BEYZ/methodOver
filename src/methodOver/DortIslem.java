41cf8fb8136d076bbda899271a3b52c337349362
ABİDE BEYZA BAYRAM <abidebeyzabayram@icloud.com>
2022-09-26 09:38:09 +0300

    method over

---
+++ src/methodOver/DortIslem.java
@@ -0,0 +1,14 @@
+package methodOver;
+
+public class DortIslem {
+
+	public int topla (int sayi1,int sayi2) {
+		return sayi1+sayi2;
+		
+	
+
+	}
+	public int topla2 (int sayi1,int sayi2,int sayı3) {
+		return sayi1+sayi2;
+}
+}
---
+++ src/methodOver/MethodOver.java
@@ -0,0 +1,12 @@
+package methodOver;
+
+public class MethodOver {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem=new DortIslem();
+	System.out.println	(dortIslem.topla(23,52));
+dortIslem.topla2(2, 10, 50);
+
+	}
+
+}
